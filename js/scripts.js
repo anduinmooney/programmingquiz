898a1666ea634416289c7e3414e1c35af0ca4cd3
Anduin Mooney <anduinmooney@gmail.com>
2017-12-01 15:22:59 -0800

    Complete quiz functionality, all answers work as they shoud.

--- js/scripts.js
+++ js/scripts.js
@@ -6,35 +6,35 @@ $(document).ready(function() {
     var question4 = $("select#question4").val();
     var question5 = $("select#question5").val();
 
-    if (question1=== 'large' && question2==='popular' && question3==='familiar' || question3==='diverse' && question4==='design' || questio4==='function' && question5==='simple') {
+    if (question1=== 'large' && question2==='popular' && (question3==='familiar' || question3==='diverse') && (question4==='design' || question4==='function') && question5==='simple') {
       $ ('#java').show();
       $ ('#ruby').hide();
       $ ('#chash').hide();
       $ ('#css').hide();
       $ ('#php').hide();
     }
-    else if (question1=== 'large' && question2==='popular' || question2==='unique' && question3==='diverse' || question3==='familiar' && question4==='function' && question5==='simple') {
+    else if (question1=== 'large' && (question2==='popular' || question2==='unique') && (question3==='diverse' || question3==='familiar') && question4==='function' && question5==='simple') {
       $ ('#java').hide();
       $ ('#ruby').hide();
       $ ('#chash').show();
       $ ('#css').hide();
       $ ('#php').hide();
     }
-    else if (question1=== 'large' || question1==='small' && question2==='unique' && question3==='diverse' || question3==='familiar' && question4==='design' && question5==='simple') {
+    else if ((question1=== 'large' || question1==='small') && question2==='unique' && (question3==='diverse' || question3==='familiar') && question4==='design' && question5==='simple') {
       $ ('#java').hide();
       $ ('#ruby').hide();
       $ ('#chash').hide();
       $ ('#css').show();
       $ ('#php').hide();
     }
-    else if (question1=== 'small' || question1==='large' && question2==='popular' && question3==='familiar' || question3==='diversity' && question4==='function' && question5==='different') {
+    else if ((question1=== 'small' || question1==='large') && question2==='popular' && (question3==='familiar' || question3==='diversity') && question4==='function' && question5==='different') {
       $ ('#java').hide();
       $ ('#ruby').show();
       $ ('#chash').hide();
       $ ('#css').hide();
       $ ('#php').hide();
     }
-    else if (question1=== 'small' || question1==='large' && question2==='unique' && question3==='diverse' || question3==='familiar' && question4==='function' && question5==='different') {
+    else if ((question1=== 'small' || question1==='large') && question2==='unique' && (question3==='diverse' || question3==='familiar') && question4==='function' && question5==='different') {
       $ ('#java').hide();
       $ ('#ruby').hide();
       $ ('#chash').hide();
